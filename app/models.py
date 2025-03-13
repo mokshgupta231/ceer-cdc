dd07f80889b1d459c9605acac9dfe3ce4dd4ed59
NAGARRO\mokshgupta <moksh.gupta@nagarro.com>
2025-03-13 13:22:42 +0530

    Approach changes

--- app/models.py
+++ app/models.py
@@ -2,16 +2,4 @@ from pydantic import BaseModel
 
 
 class RegisterRequest(BaseModel):
-    email: str
-    password: str
-    first_name: str
-    last_name: str
-
-
-class LoginRequest(BaseModel):
-    email: str
-    password: str
-
-
-class ResetPasswordRequest(BaseModel):
-    email: str
+    site_uid: str
--- app/utils.py
+++ app/utils.py
@@ -4,43 +4,14 @@ API_KEY = "4_kQKX1P9doAvP4x3kfd9N6g"
 SECRET_KEY = "AQQCnE0YaT4aqFser4a+KXqLu4X14JyWbGZHEegujWA="
 
 
-def send_register_request(email: str, password: str, first_name: str, last_name: str):
-    method = "accounts.register"
-    params = {
-        "email": email,
-        "password": password,
-        "profile": {"firstName": first_name, "lastName": last_name},
-        "finalizeRegistration": True,
-    }
-
-    request = GSRequest(API_KEY, SECRET_KEY, method, params)
-    response = request.send()
-
-    if response.getErrorCode() not in (0, 206002):
-        raise Exception(f"Error in accounts.register: {response.getErrorMessage()}")
-
-
-def send_login_request(email: str, password: str):
-    method = "accounts.login"
-    params = {"loginID": email, "password": password}
+def send_register_request(site_uid: str):
+    method = "accounts.notifyLogin"
+    params = {"siteUID": site_uid, "skipValidation": True}
 
     request = GSRequest(API_KEY, SECRET_KEY, method, params)
     response = request.send()
 
     if response.getErrorCode() != 0:
-        raise Exception(f"Error in accounts.login: {response.getErrorMessage()}")
+        raise Exception(f"Error in accounts.notifyLogin: {response.getErrorMessage()}")
 
     return response.getData()
-
-
-def send_reset_password_request(email: str):
-    method = "accounts.resetPassword"
-    params = {"loginID": email}
-
-    request = GSRequest(API_KEY, SECRET_KEY, method, params)
-    response = request.send()
-
-    if response.getErrorCode() != 0:
-        raise Exception(
-            f"Error in accounts.resetPassword: {response.getErrorMessage()}"
-        )
