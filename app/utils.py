3d633d7abeb841214e359f8dd04d72ab5ef7cfd9
NAGARRO\mokshgupta <moksh.gupta@nagarro.com>
2025-03-13 14:23:55 +0530

    bug fixes

--- app/utils.py
+++ app/utils.py
@@ -6,7 +6,7 @@ SECRET_KEY = "AQQCnE0YaT4aqFser4a+KXqLu4X14JyWbGZHEegujWA="
 
 def send_register_request(site_uid: str):
     method = "accounts.notifyLogin"
-    params = {"siteUID": site_uid, "skipValidation": True}
+    params = {"siteUID": site_uid, "skipValidation": True, "format": "json"}
 
     request = GSRequest(API_KEY, SECRET_KEY, method, params)
     response = request.send()
